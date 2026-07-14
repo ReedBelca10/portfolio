24d0494d89ccda3dae737439e4398a83e1559ff3
ReedBelca <reedbelca55@gmail.com>
2026-07-14 10:25:33 +0000

    chore: install nodemailer provider and configure email forwarding plugin

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -25,4 +25,21 @@ export default ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env.int('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: 'noreply@calebadjeoda.com',
+        defaultReplyTo: 'calebadjeoda@hotmail.com',
+      },
+    },
+  },
 });
