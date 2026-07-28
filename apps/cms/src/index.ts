c14831881f4a68e69ef91fedf096ac376271e751
ReedBelca <reedbelca55@gmail.com>
2026-07-28 17:11:39 +0000

    fix: auto-grant permissions and auto-add french locale on startup

--- apps/cms/src/index.ts
+++ apps/cms/src/index.ts
@@ -23,12 +23,16 @@ export default {
           'api::work.work.findOne',
           'api::blog.blog.find',
           'api::blog.blog.findOne',
+          'api::blog.blog.update',
+          'api::cv.cv.find',
+          'api::portfolio-info.portfolio-info.find',
+          'api::project.project.find',
+          'api::project.project.findOne',
           'api::subscriber.subscriber.create',
           'api::message.message.create',
         ];
 
         for (const action of actions) {
-          console.log(`Checking permission for ${action}...`);
           const permission = await strapi.db
             .query('plugin::users-permissions.permission')
             .findOne({ where: { role: role.id, action } });
@@ -45,6 +49,26 @@ export default {
         }
         console.log('Successfully granted public permissions for public APIs');
       }
+      
+      // Auto-add French locale if it doesn't exist
+      try {
+        const frenchLocale = await strapi.db.query('plugin::i18n.locale').findOne({
+          where: { code: 'fr' }
+        });
+        if (!frenchLocale) {
+          console.log('Adding French locale...');
+          await strapi.db.query('plugin::i18n.locale').create({
+            data: {
+              name: 'French (fr)',
+              code: 'fr',
+            }
+          });
+          console.log('Successfully added French locale');
+        }
+      } catch (localeErr) {
+        console.error('Failed to auto-add French locale:', localeErr);
+      }
+      
     } catch (err) {
       console.error('Failed to set public permissions:', err);
     }
